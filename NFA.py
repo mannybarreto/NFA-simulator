93b98d544667d1408dfddc1a38abfba86d628ec2
mannybarreto <mbarre13@lion.lmu.edu>
2019-11-27 15:51:56 -0800

    Fixes

--- NFA.py
+++ NFA.py
@@ -1,7 +1,8 @@
 class State:
-    def __init__(self, name):
+    def __init__(self, name, accept):
         self.name = name
         self.transitions = {}
+        self.accept = accept
     
     def add_transition(self, symbol, transition_state):
         self.transitions[symbol].append(transition_state)
@@ -9,3 +10,4 @@ class State:
 class NFA:
     def __init__(self, start, accepts, transitions):
         self.start = State(start)
+        self.states = {}
