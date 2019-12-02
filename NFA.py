c3faaec4c73dd5d89116a26e7d9ce462c20a6b23
mannybarreto <mbarre13@lion.lmu.edu>
2019-12-02 11:30:25 -0800

    Moved main to new file and reformatted input structure

--- NFA.py
+++ NFA.py
@@ -12,27 +12,3 @@ class NFA:
         self.start = State(name = start, accept = start in accepts)
         self.states = {}
         self.states[start] = self.start[start]
-
-if __name__ == "__main__":
-    start_and_accept = input('Enter start and accept states in following format: START=q0;ACCEPT=q2,q1').split(';')
-    entered_transitions = []
-
-    if len(start_and_accept) != 2:
-        raise ValueError('Input was invalid')
-
-    try:
-        start_state = start_and_accept[0].replace('START=', '')
-        accept_states = start_and_accept[1].replace('ACCEPT=', '').split(',')
-    except:
-        raise ValueError('Improperly formatted input')
-
-    print('Input transition states in the following format or type DONE when complete: q0:a->q1 or q0->q2')
-    while True:
-        transition = input('Transition: ')
-
-        if transition is 'DONE':
-            break
-
-        entered_transitions.append(transition)
-
-    automata = NFA(start=start_state, accepts=accept_states, transitions=entered_transitions)
---
+++ NFA_simulator.py
@@ -0,0 +1,28 @@
+if __name__ == "__main__":
+    entered_transitions = []
+
+    while True:
+        print('Enter start and accept states in following format: START=q0;ACCEPT=q2,q1')
+        start_and_accept = input('Start & Accept States: ').split(';')
+
+        if len(start_and_accept) != 2:
+            print('Improperly formatted input')
+        else:
+            try:
+                start_state = start_and_accept[0].replace('START=', '')
+                accept_states = start_and_accept[1].replace('ACCEPT=', '').split(',')
+            except:
+                print('Improperly formatted input')
+            finally:
+                break
+
+    print('Input transition states in the following format or type DONE when complete: q0:a->q1 or q0->q2')
+    while True:
+        transition = input('Transition: ')
+
+        if transition == 'DONE':
+            break
+
+        entered_transitions.append(transition)
+
+    # automata = NFA(start=start_state, accepts=accept_states, transitions=entered_transitions)
